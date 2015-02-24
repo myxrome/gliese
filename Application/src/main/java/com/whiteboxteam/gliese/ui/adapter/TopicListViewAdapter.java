cd855799d64495f4bab38d414b27a5452c48c30c
Zolotov Aleksey <myxrome@gmail.com>
2015-02-24 12:02:38 +0300

    работающий адаптер для 2х уровневого списка. не работает выбор элемента. ref #55

--- Application/src/main/java/com/whiteboxteam/gliese/ui/adapter/TopicListViewAdapter.java
+++ Application/src/main/java/com/whiteboxteam/gliese/ui/adapter/TopicListViewAdapter.java
@@ -2,9 +2,13 @@ package com.whiteboxteam.gliese.ui.adapter;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorTreeAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+import com.whiteboxteam.gliese.data.content.ApplicationContentContract;
 
 /**
  * Gliese Project.
@@ -12,38 +16,162 @@ import android.widget.CursorTreeAdapter;
  * Date: 13.02.2015
  * Time: 15:16
  */
-public class TopicListViewAdapter extends CursorTreeAdapter {
+public abstract class TopicListViewAdapter extends BaseExpandableListAdapter {
 
-    public TopicListViewAdapter(Cursor cursor, Context context) {
-        super(cursor, context);
-    }
+    private Cursor groupCursor;
+    private SparseArray<Cursor> childrenCursors = new SparseArray<>();
+    private LayoutInflater inflater;
+    private int groupIdColumnIndex;
+    private int childIdColumnIndex;
 
-    public TopicListViewAdapter(Cursor cursor, Context context, boolean autoRequery) {
-        super(cursor, context, autoRequery);
+    public TopicListViewAdapter(Context context) {
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
-    protected Cursor getChildrenCursor(Cursor groupCursor) {
+    public int getGroupCount() {
+        return groupCursor == null ? 0 : groupCursor.getCount();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        Cursor children = childrenCursors.get(groupPosition);
+        return children == null ? 0 : children.getCount();
+    }
+
+    public Cursor getGroup(int groupPosition) {
+        if (groupCursor != null) {
+            groupCursor.moveToPosition(groupPosition);
+            return groupCursor;
+        }
+        return null;
+    }
+
+    public Cursor getChild(int groupPosition, int childPosition) {
+        if (groupCursor != null) {
+            Cursor children = childrenCursors.get(groupPosition);
+            if (children != null) {
+                children.moveToPosition(childPosition);
+                return children;
+            }
+        }
+
         return null;
     }
 
     @Override
-    protected View newGroupView(Context context, Cursor cursor, boolean isExpanded, ViewGroup parent) {
-        return null;
+    public long getGroupId(int groupPosition) {
+        return getGroup(groupPosition).getLong(groupIdColumnIndex);
     }
 
     @Override
-    protected void bindGroupView(View view, Context context, Cursor cursor, boolean isExpanded) {
-
+    public long getChildId(int groupPosition, int childPosition) {
+        return getChild(groupPosition, childPosition).getLong(childIdColumnIndex);
     }
 
     @Override
-    protected View newChildView(Context context, Cursor cursor, boolean isLastChild, ViewGroup parent) {
-        return null;
+    public boolean hasStableIds() {
+        return true;
     }
 
     @Override
-    protected void bindChildView(View view, Context context, Cursor cursor, boolean isLastChild) {
-
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        Cursor cursor = getGroup(groupPosition);
+        View v;
+        if (convertView == null) {
+            v = newGroupView(parent);
+        } else {
+            v = convertView;
+        }
+        bindGroupView(v, cursor);
+        return v;
     }
+
+    private View newGroupView(ViewGroup parent) {
+        return inflater.inflate(android.R.layout.simple_expandable_list_item_1, parent, false);
+    }
+
+    private void bindGroupView(View v, Cursor cursor) {
+        TextView textView = (TextView) v.findViewById(android.R.id.text1);
+        textView.setText(cursor.getString(cursor.getColumnIndex(ApplicationContentContract.TopicGroup.NAME)));
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup
+            parent) {
+        Cursor cursor = getChild(groupPosition, childPosition);
+        View v;
+        if (convertView == null) {
+            v = newChildView(parent);
+        } else {
+            v = convertView;
+        }
+        bindChildView(v, cursor);
+        return v;
+    }
+
+    private View newChildView(ViewGroup parent) {
+        return inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+    }
+
+    private void bindChildView(View v, Cursor cursor) {
+        TextView textView = (TextView) v.findViewById(android.R.id.text1);
+        textView.setText(cursor.getString(cursor.getColumnIndex(ApplicationContentContract.Topic.NAME)));
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    public void setGroupCursor(Cursor cursor) {
+        if (groupCursor == cursor) return;
+        if (groupCursor != null) {
+            releaseChildrenCursors();
+            groupCursor.close();
+        }
+        groupCursor = cursor;
+
+        if (groupCursor != null) {
+            groupIdColumnIndex = groupCursor.getColumnIndex("_id");
+            initChildrenCursorLoaders();
+        }
+        notifyDataSetChanged();
+    }
+
+    private void releaseChildrenCursors() {
+        for (int i = 0; i < childrenCursors.size(); i++) {
+            int key = childrenCursors.keyAt(i);
+            Cursor child = childrenCursors.get(key);
+            child.close();
+        }
+        childrenCursors.clear();
+    }
+
+    private void initChildrenCursorLoaders() {
+        groupCursor.moveToPosition(-1);
+        while (groupCursor.moveToNext()) {
+            initChildrenCursorLoader(groupCursor);
+        }
+    }
+
+    protected abstract void initChildrenCursorLoader(Cursor groupCursor);
+
+    public void setChildrenCursor(int groupPosition, Cursor cursor) {
+        Cursor childrenCursor = childrenCursors.get(groupPosition);
+        if (childrenCursor == cursor) return;
+
+        if (childrenCursor != null) {
+            childrenCursors.remove(groupPosition);
+            childrenCursor.close();
+        }
+
+        if (cursor != null) {
+            childrenCursors.append(groupPosition, cursor);
+            childIdColumnIndex = cursor.getColumnIndex("_id");
+        }
+
+        notifyDataSetChanged();
+    }
+
 }
--- Application/src/main/java/com/whiteboxteam/gliese/ui/fragment/TopicDrawerFragment.java
+++ Application/src/main/java/com/whiteboxteam/gliese/ui/fragment/TopicDrawerFragment.java
@@ -13,28 +13,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
-import android.widget.SimpleCursorTreeAdapter;
 import com.whiteboxteam.gliese.R;
 import com.whiteboxteam.gliese.data.content.ApplicationContentContract;
+import com.whiteboxteam.gliese.ui.adapter.TopicListViewAdapter;
 
 public class TopicDrawerFragment extends Fragment {
 
-    private static final String TAG = "[LOADER]";
     private static final int TOPIC_GROUP_LOADER_ID = -1;
-    private SimpleCursorTreeAdapter adapter;
+    private TopicListViewAdapter adapter;
+
     private LoaderManager.LoaderCallbacks<Cursor> loaderCallbacks = new LoaderManager.LoaderCallbacks<Cursor>() {
         @Override
         public Loader<Cursor> onCreateLoader(int id, Bundle bundle) {
             switch (id) {
                 case TOPIC_GROUP_LOADER_ID:
-                    Log.d(TAG, "create topic group cursor loader");
                     return new CursorLoader(getActivity(), ApplicationContentContract.TopicGroup.CONTENT_URI, new
                             String[]{ApplicationContentContract.TopicGroup.ID, ApplicationContentContract.TopicGroup
                             .NAME}, ApplicationContentContract.TopicGroup.ACTIVE + " = ?", new String[]{"1"},
                             ApplicationContentContract.TopicGroup.ORDER);
                 default:
-                    Log.d(TAG, "create topic cursor loader with topic group " + String.valueOf(bundle.getLong
-                            (ApplicationContentContract.Topic.TOPIC_GROUP_ID)) + " and position " + String.valueOf(id));
                     Uri topicGroupUri = ContentUris.withAppendedId(ApplicationContentContract.TopicGroup.CONTENT_URI,
                             bundle.getLong(ApplicationContentContract.Topic.TOPIC_GROUP_ID));
                     return new CursorLoader(getActivity(), ApplicationContentContract.Topic.getContentUriByTopicGroup
@@ -48,11 +45,9 @@ public class TopicDrawerFragment extends Fragment {
         public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
             switch (loader.getId()) {
                 case TOPIC_GROUP_LOADER_ID:
-                    Log.d(TAG, "topic group cursor loading is finished");
                     adapter.setGroupCursor(cursor);
                     break;
                 default:
-                    Log.d(TAG, "topic cursor loading is finished, position " + String.valueOf(loader.getId()));
                     adapter.setChildrenCursor(loader.getId(), cursor);
                     break;
             }
@@ -60,7 +55,7 @@ public class TopicDrawerFragment extends Fragment {
 
         @Override
         public void onLoaderReset(Loader<Cursor> loader) {
-            Log.d(TAG, "loader reset " + String.valueOf(loader.getId()));
+
         }
 
     };
@@ -73,40 +68,36 @@ public class TopicDrawerFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        adapter = new SimpleCursorTreeAdapter(getActivity(), null, android.R.layout.simple_expandable_list_item_1,
-                new String[]{ApplicationContentContract.TopicGroup.NAME}, new int[]{android.R.id.text1}, android.R
-                .layout.simple_expandable_list_item_1, new String[]{ApplicationContentContract.Topic.NAME}, new
-                int[]{android.R.id.text1}) {
+        adapter = new TopicListViewAdapter(getActivity()) {
             @Override
-            protected Cursor getChildrenCursor(Cursor groupCursor) {
-
+            protected void initChildrenCursorLoader(Cursor groupCursor) {
                 long topicGroupId = groupCursor.getLong(groupCursor.getColumnIndex(ApplicationContentContract
                         .TopicGroup.ID));
                 Bundle bundle = new Bundle();
                 bundle.putLong(ApplicationContentContract.Topic.TOPIC_GROUP_ID, topicGroupId);
-                Log.d(TAG, "calling loader manager for topic loading with topic group " + String.valueOf
-                        (topicGroupId) + " and position " + String.valueOf(groupCursor.getPosition()));
-
-                Loader loader = getLoaderManager().getLoader(groupCursor.getPosition());
-                if (loader == null || loader.isReset()) {
-                    getLoaderManager().initLoader(groupCursor.getPosition(), bundle, loaderCallbacks);
-                } else {
-                    getLoaderManager().restartLoader(groupCursor.getPosition(), bundle, loaderCallbacks);
-                }
-
-                return null;
+                getLoaderManager().initLoader(groupCursor.getPosition(), bundle, loaderCallbacks);
             }
         };
-        ExpandableListView listView = (ExpandableListView) view.findViewById(R.id.drawer_list_view);
-        listView.setAdapter(adapter);
-        Log.d(TAG, "calling loader manager for topic groups");
 
-        Loader loader = getLoaderManager().getLoader(TOPIC_GROUP_LOADER_ID);
-        if (loader == null || loader.isReset()) {
-            getLoaderManager().initLoader(TOPIC_GROUP_LOADER_ID, null, loaderCallbacks);
-        } else {
-            getLoaderManager().restartLoader(TOPIC_GROUP_LOADER_ID, null, loaderCallbacks);
-        }
+        final ExpandableListView listView = (ExpandableListView) view.findViewById(R.id.drawer_list_view);
+        listView.setAdapter(adapter);
+        listView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long
+                    id) {
+                listView.expandGroup(groupPosition);
+                boolean result = listView.setSelectedChild(groupPosition, childPosition, true);
+                listView.setSelection(listView.getFlatListPosition(ExpandableListView.getPackedPositionForChild
+                        (groupPosition, childPosition)));
+                Log.d("[CLICK]", String.valueOf(result));
+                Log.d("[CLICK]", String.valueOf(listView.getSelectedId()));
+//                listView.setItemChecked(listView.getFlatListPosition(ExpandableListView.getPackedPositionForChild
+// (groupPosition, childPosition)), true);
+                return true;
+            }
+        });
+
+        getLoaderManager().initLoader(TOPIC_GROUP_LOADER_ID, null, loaderCallbacks);
     }
 
 }
