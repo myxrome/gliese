01da81f5247522051a4ed0d93108b866af026308
Zolotov Aleksey <myxrome@gmail.com>
2015-03-10 12:17:01 +0300

    изменилась логика работы загрузчика картинок. фоновая работает только для иконок и только при удовлетворяющем заряде батареи. ref #55

--- Application/src/main/java/com/whiteboxteam/gliese/data/sync/image/ImageUploadService.java
+++ Application/src/main/java/com/whiteboxteam/gliese/data/sync/image/ImageUploadService.java
@@ -3,7 +3,9 @@ package com.whiteboxteam.gliese.data.sync.image;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import com.whiteboxteam.gliese.data.content.ApplicationContentContract;
@@ -27,8 +29,9 @@ public class ImageUploadService extends Service {
     private static final long KEEP_ALIVE_TIME = 0L;
     private static final int DEFAULT_INITIAL_CAPACITY = 11;
     private static final int DEFAULT_UPLOAD_PRIORITY = 0;
+    private static final int MIN_BATTERY_LEVEL = 10;
 
-    private ThreadPoolExecutor imageBackgroundPool;
+    //    private ThreadPoolExecutor imageBackgroundPool;
     private ThreadPoolExecutor imageForegroundPool;
     private ThreadPoolExecutor thumbBackgroundPool;
     private ThreadPoolExecutor thumbForegroundPool;
@@ -67,8 +70,8 @@ public class ImageUploadService extends Service {
     public void onCreate() {
         super.onCreate();
         UploadTaskComparator uploadTaskComparator = new UploadTaskComparator();
-        imageBackgroundPool = new ThreadPoolExecutor(DOWNLOAD_POOL_SIZE, DOWNLOAD_POOL_SIZE, KEEP_ALIVE_TIME,
-                TimeUnit.MILLISECONDS, new PriorityBlockingQueue<>(DEFAULT_INITIAL_CAPACITY, uploadTaskComparator));
+//        imageBackgroundPool = new ThreadPoolExecutor(DOWNLOAD_POOL_SIZE, DOWNLOAD_POOL_SIZE, KEEP_ALIVE_TIME,
+//                TimeUnit.MILLISECONDS, new PriorityBlockingQueue<>(DEFAULT_INITIAL_CAPACITY, uploadTaskComparator));
         imageForegroundPool = new ThreadPoolExecutor(DOWNLOAD_POOL_SIZE * 2, DOWNLOAD_POOL_SIZE * 2, KEEP_ALIVE_TIME,
                 TimeUnit.MILLISECONDS, new PriorityBlockingQueue<>(DEFAULT_INITIAL_CAPACITY, uploadTaskComparator));
         thumbBackgroundPool = new ThreadPoolExecutor(DOWNLOAD_POOL_SIZE, DOWNLOAD_POOL_SIZE, KEEP_ALIVE_TIME,
@@ -99,29 +102,33 @@ public class ImageUploadService extends Service {
                 }
 
             } else {
-                Cursor values = getContentResolver().query(ApplicationContentContract.Value.CONTENT_URI, new
-                        String[]{ApplicationContentContract.Value.ID, ApplicationContentContract.Value
-                        .REMOTE_THUMB_URI}, ApplicationContentContract.Value.LOCAL_THUMB_URI + " IS NULL", null, null);
+                if (getBatteryLevel() > MIN_BATTERY_LEVEL) {
+                    Cursor values = getContentResolver().query(ApplicationContentContract.Value.CONTENT_URI, new
+                            String[]{ApplicationContentContract.Value.ID, ApplicationContentContract.Value
+                            .REMOTE_THUMB_URI}, ApplicationContentContract.Value.LOCAL_THUMB_URI + " IS NULL", null,
+                            null);
 
-                if (values != null) {
-                    while (values.moveToNext()) {
-                        Runnable task = new ValueThumbUploadTask(this, values.getLong(0), DEFAULT_UPLOAD_PRIORITY);
-                        thumbBackgroundPool.execute(task);
+                    if (values != null) {
+                        while (values.moveToNext()) {
+                            Runnable task = new ValueThumbUploadTask(this, values.getLong(0), DEFAULT_UPLOAD_PRIORITY);
+                            thumbBackgroundPool.execute(task);
+                        }
+                        values.close();
                     }
-                    values.close();
                 }
 
-                Cursor promos = getContentResolver().query(ApplicationContentContract.Promo.CONTENT_URI, new
-                        String[]{ApplicationContentContract.Promo.ID, ApplicationContentContract.Promo
-                        .REMOTE_IMAGE_URI}, ApplicationContentContract.Promo.LOCAL_IMAGE_URI + " IS NULL", null, null);
-
-                if (promos != null) {
-                    while (promos.moveToNext()) {
-                        Runnable task = new PromoImageUploadTask(this, promos.getLong(0), DEFAULT_UPLOAD_PRIORITY);
-                        imageBackgroundPool.execute(task);
-                    }
-                    promos.close();
-                }
+//                Cursor promos = getContentResolver().query(ApplicationContentContract.Promo.CONTENT_URI, new
+//                        String[]{ApplicationContentContract.Promo.ID, ApplicationContentContract.Promo
+//                        .REMOTE_IMAGE_URI}, ApplicationContentContract.Promo.LOCAL_IMAGE_URI + " IS NULL", null,
+// null);
+//
+//                if (promos != null) {
+//                    while (promos.moveToNext()) {
+//                        Runnable task = new PromoImageUploadTask(this, promos.getLong(0), DEFAULT_UPLOAD_PRIORITY);
+//                        imageBackgroundPool.execute(task);
+//                    }
+//                    promos.close();
+//                }
             }
         }
 
@@ -133,13 +140,25 @@ public class ImageUploadService extends Service {
         thumbForegroundPool.shutdownNow();
         thumbBackgroundPool.shutdownNow();
         imageForegroundPool.shutdownNow();
-        imageBackgroundPool.shutdownNow();
+//        imageBackgroundPool.shutdownNow();
         super.onDestroy();
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public float getBatteryLevel() {
+        Intent batteryIntent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        if (batteryIntent != null) {
+            int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            if (level > -1 && scale > -1) {
+                return ((float) level / (float) scale) * 100.0f;
+            }
+        }
+        return 0.0f;
     }
 
     private static abstract class Parameters {
