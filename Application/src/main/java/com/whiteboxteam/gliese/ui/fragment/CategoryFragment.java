c3cb928e32e6b3e95e5db3e8e3cea6e5d9913dfc
Zolotov Aleksey <myxrome@gmail.com>
2015-03-27 11:40:15 +0300

    сообщение о пустой категории. resolve #79

--- Application/src/main/java/com/whiteboxteam/gliese/ui/fragment/CategoryFragment.java
+++ Application/src/main/java/com/whiteboxteam/gliese/ui/fragment/CategoryFragment.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import com.whiteboxteam.gliese.R;
 import com.whiteboxteam.gliese.data.content.ApplicationContentContract;
 import com.whiteboxteam.gliese.data.entity.FactEntity;
@@ -33,26 +34,33 @@ import java.util.*;
 public class CategoryFragment extends Fragment {
 
     private static final int VALUE_LOADER_ID = 18000;
-    private FactHelper factHelper;
-    private FactEntity categoryTimer;
-    private int loaderId;
-    private long categoryId;
+    private FactHelper               factHelper;
+    private FactEntity               categoryTimer;
+    private int                      loaderId;
+    private long                     categoryId;
     private ValueRecyclerViewAdapter adapter;
+    private TextView                 noValueText;
     private List<RecyclerView> recyclerViews = new ArrayList<>();
-    private Timer timer = new Timer();
-    private boolean scheduled = false;
+    private Timer              timer         = new Timer();
+    private boolean            scheduled     = false;
 
     private LoaderManager.LoaderCallbacks<Cursor> loaderCallbacks = new LoaderManager.LoaderCallbacks<Cursor>() {
         @Override
         public Loader<Cursor> onCreateLoader(int id, Bundle args) {
             if (loaderId == id) {
-                Uri categoryUri = ContentUris.withAppendedId(ApplicationContentContract.Category.CONTENT_URI,
-                        categoryId);
-                return new CursorLoader(getActivity(), ApplicationContentContract.Value.getContentUriByCategory
-                        (categoryUri), new String[]{ApplicationContentContract.Value.ID, ApplicationContentContract
-                        .Value.NAME, ApplicationContentContract.Value.OLD_PRICE, ApplicationContentContract.Value
-                        .DISCOUNT, ApplicationContentContract.Value.NEW_PRICE, ApplicationContentContract.Value
-                        .LOCAL_THUMB_URI, ApplicationContentContract.Value.URL}, ApplicationContentContract.Value.ACTIVE + " = ?", new String[]{"1"}, ApplicationContentContract.Value.DISCOUNT + " DESC");
+                Uri categoryUri = ContentUris
+                        .withAppendedId(ApplicationContentContract.Category.CONTENT_URI, categoryId);
+                return new CursorLoader(getActivity(),
+                                        ApplicationContentContract.Value.getContentUriByCategory(categoryUri),
+                                        new String[] {ApplicationContentContract.Value.ID,
+                                                      ApplicationContentContract.Value.NAME,
+                                                      ApplicationContentContract.Value.OLD_PRICE,
+                                                      ApplicationContentContract.Value.DISCOUNT,
+                                                      ApplicationContentContract.Value.NEW_PRICE,
+                                                      ApplicationContentContract.Value.LOCAL_THUMB_URI,
+                                                      ApplicationContentContract.Value.URL},
+                                        ApplicationContentContract.Value.ACTIVE + " = ?", new String[] {"1"},
+                                        ApplicationContentContract.Value.DISCOUNT + " DESC");
             }
             return null;
         }
@@ -62,6 +70,9 @@ public class CategoryFragment extends Fragment {
             if (loaderId == loader.getId()) {
                 adapter.changeCursor(data);
                 if (getUserVisibleHint()) {
+                    int visibility = data.getCount() > 0 ? View.INVISIBLE : View.VISIBLE;
+                    noValueText.setVisibility(visibility);
+
                     scheduleShuffleTimerTask();
                 }
             }
@@ -163,6 +174,7 @@ public class CategoryFragment extends Fragment {
         initRecycleView(view.findViewById(R.id.recycler_view_1));
         initRecycleView(view.findViewById(R.id.recycler_view_2));
         initRecycleView(view.findViewById(R.id.recycler_view_3));
+        noValueText = (TextView) view.findViewById(R.id.no_value_text);
 
         return view;
     }
